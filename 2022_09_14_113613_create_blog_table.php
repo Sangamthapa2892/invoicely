860b0a669ec4fad94922a1a9844554a92d196386
Sangamthapa2892 <111414767+Sangamthapa2892@users.noreply.github.com>
2022-09-14 19:45:35 +0545

    db blog
    db blog

---
+++ 2022_09_14_113613_create_blog_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title',60);
+            $table->longText('description',150);
+            $table->tinyInteger('category_id');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+};
---
+++ 2022_09_14_124740_create_contact_us_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->integer('id')->notnull();
+            $table->char('name',20);
+            $table->bigInteger('contact_number',20);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_us');
+    }
+};
